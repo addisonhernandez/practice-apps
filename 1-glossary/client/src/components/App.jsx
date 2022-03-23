3fe372a336bd27ac24b75c3fd8e610854f375e3c
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-23 10:56:05 -0400

    Initialize boilerplate App component

---
+++ 1-glossary/client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <p>Hello, world!</p>
+      </>
+    );
+  }
+}
+
+export default App;
