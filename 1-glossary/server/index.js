888b7848919c26bb3a1a757e68579c8484881f51
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-23 12:41:57 -0400

    Lay out initial routes

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require('./db');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// route GET request to root
+app.get('/', (req, res) => {
+  console.log('Serving GET to /');
+  db.find({}).sort({ word: 'asc' }).exec()
+    .then((results) => {
+      console.log(results);
+      return results;
+    })
+    .then((entries) => res.status(200).send(entries))
+    .catch((err) => {
+      console.log('Error while reading from the database');
+      console.error(err);
+    });
+});
+
+// route POST request to /glossary
+app.post('/glossary', (req, res) => {
+  // TODO:
+  res.status(404).end();
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
