8e5a954119974c83380c4aec566be8b33d604aee
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-23 16:46:36 -0400

    Add getGlossaryEntries and saveEntry methods

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { titleCase } = require('../utils');
 
 // 1. Use mongoose to establish a connection to MongoDB
 mongoose
@@ -19,8 +20,23 @@ const glossarySchema = new mongoose.Schema({
 
 // NOTE: Methods can be added directly to the schema, before creating the model
 
-// 3. Export the models
 const Glossary = mongoose.model('Glossary', glossarySchema);
 
+const getGlossaryEntries = function () {
+  return Glossary.find({}).sort({ word: 'asc' }).exec();
+};
+
+const saveEntry = function (entry) {
+  const word = titleCase(entry.word);
+  const definition = titleCase(entry.definition);
+
+  return Glossary.findOneAndUpdate(
+    { word: entry.word }, // filter
+    { word, definition }, // object to insert
+    { upsert: true } // option flags
+  ).exec();
+};
+
+// 3. Export the models
 // 4. Import the models into any modules that need them
-module.exports = Glossary;
+module.exports = { getGlossaryEntries, saveEntry };
