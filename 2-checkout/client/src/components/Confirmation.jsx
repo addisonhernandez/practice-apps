779f2f4cf5e4bf36d2e1d59dd27258856a2555c4
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-24 17:35:47 -0400

    Initial layout of Confirmation page

--- 2-checkout/client/src/components/Confirmation.jsx
+++ 2-checkout/client/src/components/Confirmation.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
 
-const Confirmation = (props) => (
-  <div className="confirmation"></div>
-);
+const Confirmation = (props) => {
+  const { formOneData, formTwoData, formThreeData } = props.formData;
+  return (
+    <div className="confirmation">
+      <h1>Confirmation</h1>
+      <h2>Account</h2>
+      {Object.keys(formOneData).map((key) => (
+        <div key={key}>
+          <span>{key}: </span>
+          <span>{formOneData[key]}</span>
+          <br />
+        </div>
+      ))}
+      {/* <div>{JSON.stringify(formOneData)}</div> */}
+      <h2>Shipping</h2>
+      <p>Shipping address</p>
+      {Object.keys(formTwoData.shippingAddress).map((key) => (
+        <div key={key}>
+          <span>{formOneData[key]}</span>
+          <br />
+        </div>
+      ))}
+      <div>{JSON.stringify(formTwoData)}</div>
+      <h2>Payment</h2>
+      <div>{JSON.stringify(formThreeData)}</div>
+    </div>
+  );
+};
 
 export default Confirmation;
