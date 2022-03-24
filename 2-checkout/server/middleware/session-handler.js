ff7e5bebdf6da1f2afdd3aa7e4c339c676aea854
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-24 17:36:08 -0400

    Simplify cookie string manipulation

--- 2-checkout/server/middleware/session-handler.js
+++ 2-checkout/server/middleware/session-handler.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (req, res, next) => {
   /**
@@ -7,23 +7,18 @@ module.exports = (req, res, next) => {
    *
    */
 
-  let cookieString = req.get("Cookie") || "";
+  let cookieString = req.get('Cookie') || '';
 
-  parsedCookies = cookieString.split("; ").reduce((cookies, cookie) => {
-    if (cookie.length) {
-      let index = cookie.indexOf("=");
-      let key = cookie.slice(0, index);
-      let token = cookie.slice(index + 1);
-      cookies[key] = token;
-    }
-    return cookies;
-  }, {});
+  parsedCookies = Object.fromEntries(
+    cookieString.split('; ')
+      .map((cookie) => cookie.split('='))
+  );
 
   if (parsedCookies.s_id) {
     req.session_id = parsedCookies.s_id;
   } else {
     req.session_id = uuidv4();
-    res.cookie("s_id", req.session_id);
+    res.cookie('s_id', req.session_id);
   }
 
   next();
