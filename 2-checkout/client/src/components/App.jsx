5b361e0c6cd497c2242cb9231a5a6d7ba11a8d03
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-24 15:55:54 -0400

    Add conditional form rendering
    Refactor form submission methods into a single form submit method

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import FormOne from './FormOne.jsx';
+import FormTwo from './FormTwo.jsx';
+import FormThree from './FormThree.jsx';
+import Confirmation from './Confirmation.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -30,19 +34,53 @@ class App extends Component {
     };
   }
 
+  handleFormSubmit(formData, formName, nextFormIndex) {
+    const stateChange = {};
+
+    stateChange[formName] = formData;
+    stateChange.currentForm = nextFormIndex;
+
+    this.setState(stateChange);
+  }
+
   componentDidMount() {}
 
   render() {
-    return (
-      <>
-        <p>Hello, World!</p>
-        <p>
-          <code>
-            Page Cookie: {JSON.stringify(document.cookie, undefined, '\t')}
-          </code>
-        </p>
-      </>
-    );
+    switch (this.state.currentForm) {
+      case 1:
+        return (
+          <FormOne
+            submitForm={(formData) =>
+              this.handleFormSubmit(formData, 'formOneData', 2)
+            }
+          />
+        );
+      case 2:
+        return (
+          <FormTwo
+            submitForm={(formData) =>
+              this.handleFormSubmit(formData, 'formTwoData', 3)
+            }
+          />
+        );
+      case 3:
+        return (
+          <FormThree
+            submitForm={(formData) =>
+              this.handleFormThree(formData, 'formThreeData', 4)
+            }
+          />
+        );
+      case 4:
+        return <Confirmation />;
+      case 0:
+      default:
+        return (
+          <button onClick={() => this.setState({ currentForm: 1 })}>
+            Checkout 🛒
+          </button>
+        );
+    }
   }
 }
 
