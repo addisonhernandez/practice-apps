18a0b5e650254efa24519ee61d98a9c61900bf09
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-24 16:06:01 -0400

    Implement FormOne fields and base functionality

--- 2-checkout/client/src/components/FormOne.jsx
+++ 2-checkout/client/src/components/FormOne.jsx
@@ -5,16 +5,52 @@ class FormOne extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    };
   }
 
-  componentDidMount() {};
+  updateStateFromEvent(event, prop) {
+    const stateChange = {};
+    stateChange[prop] = event.target.value;
+    this.setState(stateChange);
+  }
+
+  componentDidMount() {}
 
   render() {
-    return (<></>);
-  };
+    return (
+      <div className="form-one">
+        <span>Name: </span>
+        <input
+          value={this.state.name}
+          onChange={(e) => this.updateStateFromEvent(e, 'name')}
+        ></input>
+        <br />
+        <span>Email: </span>
+        <input
+          value={this.state.email}
+          onChange={(e) => this.updateStateFromEvent(e, 'email')}
+        ></input>
+        <br />
+        <span>Password: </span>
+        <input
+          value={this.state.password}
+          onChange={(e) => this.updateStateFromEvent(e, 'password')}
+        ></input>
+        <br />
+        <button onClick={() => this.props.submitForm(this.state)}>
+          Next 👉
+        </button>
+      </div>
+    );
+  }
 }
 
-FormOne.propTypes = {};
+FormOne.propTypes = {
+  submitForm: PropTypes.func.isRequired,
+};
 
 export default FormOne;
