9e9d91aa059d2eb3ac6923b9c1d8c12f3bbb550f
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-25 07:33:20 -0400

    Add form view components

--- 2-checkout/client/src/components/Confirmation.jsx
+++ 2-checkout/client/src/components/Confirmation.jsx
@@ -1,30 +1,56 @@
 import React from 'react';
 
+const FormOneView = ({ formOneData }) => (
+  <>
+    <h2>Account</h2>
+    {Object.keys(formOneData).map((key) => (
+      <div key={key}>
+        <span>{key}: </span>
+        <span>{formOneData[key]}</span>
+        <br />
+      </div>
+    ))}
+  </>
+);
+
+const FormTwoView = ({ formTwoData }) => {
+  const { shippingAddress } = formTwoData;
+  return (
+    <>
+      <h2>Shipping</h2>
+      <p>Shipping address</p>
+      {Object.keys(shippingAddress).map((key) => (
+        <div key={key}>
+          <span>{shippingAddress[key]}</span>
+          <br />
+        </div>
+      ))}
+      <p>Phone Number: {formTwoData.phoneNumber}</p>
+    </>
+  );
+};
+
+const FormThreeView = ({ formThreeData }) => (
+  <>
+    <h2>Payment</h2>
+    {Object.keys(formThreeData).map((key) => (
+      <div key={key}>
+        <span>{key}: </span>
+        <span>{formThreeData[key]}</span>
+      </div>
+    ))}
+  </>
+);
+
 const Confirmation = (props) => {
   const { formOneData, formTwoData, formThreeData } = props.formData;
   return (
     <div className="confirmation">
       <h1>Confirmation</h1>
-      <h2>Account</h2>
-      {Object.keys(formOneData).map((key) => (
-        <div key={key}>
-          <span>{key}: </span>
-          <span>{formOneData[key]}</span>
-          <br />
-        </div>
-      ))}
-      {/* <div>{JSON.stringify(formOneData)}</div> */}
-      <h2>Shipping</h2>
-      <p>Shipping address</p>
-      {Object.keys(formTwoData.shippingAddress).map((key) => (
-        <div key={key}>
-          <span>{formOneData[key]}</span>
-          <br />
-        </div>
-      ))}
-      <div>{JSON.stringify(formTwoData)}</div>
-      <h2>Payment</h2>
-      <div>{JSON.stringify(formThreeData)}</div>
+      <FormOneView formOneData={formOneData} />
+      <FormTwoView formTwoData={formTwoData} />
+      <FormThreeView formThreeData={formThreeData} />
+      <button>Purchase</button>
     </div>
   );
 };
